decedbfa7dc65884852b88c9545ff1ed39200d62
ManuelTgn <manu.tognon@gmail.com>
2022-02-11 12:34:55 +0100

    fix mail sender's __main__

--- pages/send_mail.py
+++ pages/send_mail.py
@@ -1,3 +1,26 @@
+"""Send email to notify the user that CRISPRme analysis was completed and
+the corresponding results are available to be visualized and explored.
+
+Structure of email.txt
+    - Contact
+    - link to CRISPRme job
+    - Job submission date
+
+TODO: avoid shell and call send_mail() in other python scripts
+TODO: add run parameters to mail (job date + other params)
+"""
+
+from .results_page_utils import MAIL_SUBJECT, MAIL_SENDER, SSL_PORT
+
+from email.message import EmailMessage
+
+import smtplib
+import ssl
+import sys
+import os
+
+
+
 # import for send mail
 # Structure of email.txt
 # destinatario
@@ -7,51 +30,72 @@
 
 # argv[1] is job directory, eg Results/72C1MNXDWF
 
-import sys
-import smtplib
-from email.message import EmailMessage
-import ssl
+
+def send_mail() -> None:
+    """Send the email to notify the user that his/her CRISPRme is 
+    complete.
+    
+    ...
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+
+    try:
+        # in future the folowing 2 lines should be removed
+        job_directory = sys.argv[1]
+        assert os.path.exists(job_directory)
+        with open(os.path.join(job_directory), "email.txt", mode="r") as handle:
+            # build mail message
+            mail_content = handle.read().strip().split("--OTHEREMAIL--")
+            for em in mail_content:
+                msg = EmailMessage()  # create the email object
+                em = em.strip().split("\n")  # get mail fields
+                msg["To"] = em[0]
+                job_link = em[1]
+                submission_date = em[2]
+                msg["Subject"] = MAIL_SUBJECT
+                msg["From"] = MAIL_SENDER
+                mail_body = "Your CRISPRme job is completed.\n\n"
+                mail_body += f"visit the following link to view the report:\n{job_link}"
+                msg.set_content(mail_body)
+                print("EMAIL SENT")
+                # univr settings
+                # server = smtplib.SMTP(host="smtp.univr.it", port=25)
+                # server.ehlo_or_helo_if_needed()
+                # server.send_message(msg, from_addr='crisprme-job@crisprme.di.univr.it')
+                # server.quit()
+                # gmail settings
+                port = SSL_PORT  # For SSL (used for gmail account)
+                # Create a secure SSL context
+                context = ssl.create_default_context()
+                with smtplib.SMTP_SSL(
+                    "<INSERT SMTP SERVER>", port, context=context
+                ) as server:
+                    server.login("<USERNAME>", "<PASSWORD>")
+                    server.send_message(msg)
+    except OSError as e:
+        raise e
 
 
-def sendMail():
-    with open(sys.argv[1] + '/email.txt', 'r') as e:
-        # message building
-        all_content = e.read().strip().split('--OTHEREMAIL--')
-        for em in all_content:
-
-            msg = EmailMessage()
-            em = em.strip().split('\n')
-            msg['To'] = em[0]
-            job_link = em[1]
-            date_submission = em[2]
-            msg['Subject'] = 'CRISPRme - Job completed'
-
-            msg['From'] = '<SENDER OF RESULT MAIL>'
-            content_email = 'The requested job is completed, visit the following link ' + \
-                job_link + ' to view the report.'
-
-            # TODO add Parameters section with date and other parameters
-            msg.set_content(content_email)
-
-            print('EMAIL SENT')
-
-            # univr settings
-            # server = smtplib.SMTP(host="smtp.univr.it", port=25)
-            # server.ehlo_or_helo_if_needed()
-            # server.send_message(msg, from_addr='crisprme-job@crisprme.di.univr.it')
-            # server.quit()
-
-            # gmail settings
-            port = 465  # For SSL (used for gmail account)
-            # Create a secure SSL context
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL("<INSERT SMTP SERVER>", port, context=context) as server:
-                server.login("<USERNAME>", '<PASSWORD>')
-                server.send_message(msg)
+# call the above funtion
+# TODO: avoid calling through bash
+def main():
+    """Call the above function and send mails to notify user about 
+    CRSPRme job completion.
+    """
+    
+    try:
+        send_mail()
+    except:
+        sys.stderr.write("No email request by the user")
 
 
-# function call
-try:
-    sendMail()
-except:
-    print('NO mail requested by user')
+# entry point
+if __name__ == "__main__":
+    main()
