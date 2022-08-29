77582d989177fbdcb648b5a0cb97a5ca19dd2c7d
Samuele Cancellieri <32717860+samuelecancellieri@users.noreply.github.com>
2022-08-29 17:09:16 +0200

    test split with bed input

--- pages/main_page.py
+++ pages/main_page.py
@@ -1172,7 +1172,8 @@ def check_input(
                 for line in seq.split("\n"):
                     if not line.strip():
                         continue
-                    line_split = line.strip().split()
+                    # line_split = line.strip().split()
+                    line_split = re.split("|;|-|,|.|[\s]+", line.strip())
                     seq_read = f"{line_split[0]}:{line_split[1]}-{line_split[2]}"
                     assert bool(seqname)
                     assert bool(seq_read)
@@ -2096,7 +2097,8 @@ def update_base_editing_dropdown(
                 for line in seq.split("\n"):
                     if not line:
                         continue
-                    line_split = line.strip().split()
+                    # line_split = line.strip().split()
+                    line_split = re.split("|;|-|,|.|[\s]+", line.strip())
                     seq_read = f"{line_split[0]}:{line_split[1]}-{line_split[2]}"
                     seq_read = extract_seq.extractSequence(
                         seqname, seq_read, genome.replace(" ", "_")
