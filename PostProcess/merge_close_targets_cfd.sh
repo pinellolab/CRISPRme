fee880e1883db456960a7f9fada704a4ec095093
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2024-07-29 16:24:36 +0200

    optimized memory usage during targets merge

--- PostProcess/merge_close_targets_cfd.sh
+++ PostProcess/merge_close_targets_cfd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e  # capture any failure
+# set -e  # capture any failure
 
 fileIn=$1
 fileOut=$2
@@ -28,8 +28,9 @@ echo "Sorting done in $(($ENDTIME - $STARTTIME)) seconds"
 # rm $fileIn.sorted.tmp
 echo "Merging contiguous targets"
 # python remove_contiguous_samples_cfd.py $fileIn.sorted $fileOut $thresh $chrom $position $total $true_guide $snp_info $cfd
-python remove_contiguous_samples_cfd.py $fileIn $fileOut $thresh $chrom $position $total $true_guide $snp_info $cfd $sort_pivot $sorting_criteria_scoring $sorting_criteria || {
-    echo "CRISPRme ERROR: contigous SNP removal failed (script: ${0} line $((LINENO-1)))" >&2
-	exit 1
-}
+python remove_contiguous_samples.py $fileIn $fileOut $thresh $chrom $position $total $true_guide $snp_info $cfd $sort_pivot $sorting_criteria_scoring $sorting_criteria
+# python remove_contiguous_samples_cfd_new.py $fileIn $fileOut $thresh $chrom $position $total $true_guide $snp_info $cfd $sort_pivot $sorting_criteria_scoring $sorting_criteria || {
+#     echo "CRISPRme ERROR: contigous SNP removal failed (script: ${0} line $((LINENO-1)))" >&2
+# 	exit 1
+# }
 # rm $fileIn.sorted
